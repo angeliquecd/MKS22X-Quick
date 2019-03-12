4008baebf3fc37286b9f216b7e805135aa10687e
angeliquecd <acharles-davis00@stuy.edu>
2019-03-12 09:59:57 -0400

    added easy implementations from class

--- Quick.java
+++ Quick.java
@@ -2,26 +2,38 @@ import java.util.Random;
 public class Quick {
   public static int partition(int[] data,int start,int end){
     Random rng = new Random();
-    int index=start+(Math.abs(rng.nextInt()%(end-start+1)));//picks random index
-    int pivot = data[index];
-  //  System.out.println("pivot: "+ pivot);
+    //int index=start+(Math.abs(rng.nextInt()%(end-start+1)));//picks random index
+    int lowest;
+    int highest;
+    if (data[0]<data[data.length-1]) lowest= data[0];
+    else lowest = data[data.length-1];
+    if (lowest>data[data.length/2]) lowest= data[data.length/2];
+    if (data[0]>data[data.length-1]) highest=data[0];
+    else highest=data[data.length-1];
+    if (highest<data[data.length/2]) highest=data[data.length/2];
+    int index = data[0]+data[data.length-1]+data[data.length/2]-lowest-highest;
+    int pivot = data[index];  System.out.println("pivot: "+ pivot);
     int stored=0;
     data[index]=data[start];
     data[start]=pivot;
     int endy=end;
+    int move=-1;
     for (int inc=0;start+1+inc<=end;inc++){
-      if (data[start+1+inc]>pivot){
+      if (data[start+1+inc]==pivot){
+        move = Math.abs(rng.nextInt()%2);
+      }
+      if (data[start+1+inc]>pivot ||move==0){
         stored=data[start+1+inc];
         data[inc+start+1]=data[end];
         data[end]=stored;//swaps with last one
         inc--;
         end--;//so that it doesn't go on forever
-      }}
-      //printarray(data);}
+      }
+      move=-1;
+    }//printarray(data);
     //  System.out.println("done parsing");
     for (int i=start+1;i<=endy;i++){
-      if (data[i]>pivot){
-//System.out.println("in it");
+      if (data[i]>pivot){//System.out.println("in it");
         data[start]=data[i-1];
         data[i-1]=pivot;//finds correct spot
       //  System.out.println("returning as index: "+ (i-1));
@@ -52,6 +64,12 @@ public class Quick {
    }
    return -1;//unreachable
  }
+ /*Modify the array to be in increasing order.
+ */
+ public static void quicksort(int[] data){
+   int index = partition(data,start,end);
+   if (index==)
+ }
  public static void toString(int[] data){
    for (int a: data){
      System.out.print(a+" ");
@@ -70,6 +88,11 @@ public class Quick {
    for (int i=0;i<10000;i++){
      ary2[i]=rng.nextInt()%10000;
    }
+   int [] ary6 = new int[50];
+   for (int i=0;i<50;i++){
+     ary6[i]=4;
+   }
+   System.out.println(quickselect(ary6,20));
     System.out.println(quickselect(ary2,5));//23
     int[] ary3 = new int[100000];
     Random rng1 = new Random();
